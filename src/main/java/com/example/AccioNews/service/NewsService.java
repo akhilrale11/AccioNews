c5771833ed79df0d276a9b176bb1284df0701df4
AkhilR111 <130051236+AkhilR111@users.noreply.github.com>
2026-09-18 22:00:29 +0530

    Fix Java version and implement NewsService

--- src/main/java/com/example/AccioNews/service/NewsService.java
+++ src/main/java/com/example/AccioNews/service/NewsService.java
@@ -1,14 +1,22 @@
 package com.example.AccioNews.service;
 
+import com.example.AccioNews.dto.NewsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Service
 public class NewsService {
 
     @Autowired
-    NewsService newsService;
+    private RestTemplate restTemplate;
 
-}
-public Object getNewsByCountry( String country, String apiKey) {
-    String url = prepareUrl()
+    public NewsResponse getNewsByCountry(String country, String apiKey) {
+        String url = prepareUrl(country, apiKey);
+        return restTemplate.getForObject(url, NewsResponse.class);
+    }
+
+    private String prepareUrl(String country, String apiKey) {
+        return "https://newsapi.org/v2/top-headlines?country=" + country + "&apiKey=" + apiKey;
+    }
 }
